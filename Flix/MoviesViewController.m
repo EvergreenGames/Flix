eb04626b137de9eaf45fb8813b802164d928d412
Ruben Green <ragreen@fb.com>
2020-06-24 15:39:59 -0700

    Movie poster loading

--- Flix/MoviesViewController.m
+++ Flix/MoviesViewController.m
@@ -8,12 +8,13 @@
 
 #import "MoviesViewController.h"
 #import "MovieCell.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-
 @property (nonatomic, strong) NSArray* movies;
+@property (nonatomic, strong) UIRefreshControl* refreshControl;
 
 @end
 
@@ -26,6 +27,15 @@
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     
+    [self fetchMovieList];
+    
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchMovieList) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
+    
+}
+
+- (void) fetchMovieList {
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -38,17 +48,15 @@
 
                self.movies = dataDictionary[@"results"];
                for(NSDictionary* movie in self.movies){
-                   NSLog(movie[@"title"]);
+                   NSLog(@"%@", movie[@"title"]);
                }
                
                [self.tableView reloadData];
-               // TODO: Get the array of movies
-               // TODO: Store the movies in a property to use elsewhere
-               // TODO: Reload your table view data
            }
+        
+        [self.refreshControl endRefreshing];
        }];
     [task resume];
-    
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
@@ -62,6 +70,15 @@
     cell.titleLabel.text = movie[@"title"];
     cell.descLabel.text = movie[@"overview"];
 
+    NSString* baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString* posterURLString = movie[@"poster_path"];
+    NSString* fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+    
+    NSURL* posterURL = [NSURL URLWithString:fullPosterURLString];
+    
+    cell.coverImageView.image = nil;
+    [cell.coverImageView setImageWithURL:posterURL];
+    
     return cell;
 }
 
