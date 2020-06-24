a808e36d83f7bf4cf56b0769ec3882752cdf9945
Ruben Green <ragreen@fb.com>
2020-06-24 13:28:27 -0700

    table view setup

--- Flix/MoviesViewController.m
+++ Flix/MoviesViewController.m
@@ -8,7 +8,9 @@
 
 #import "MoviesViewController.h"
 
-@interface MoviesViewController ()
+@interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @property (nonatomic, strong) NSArray* movies;
 
@@ -19,6 +21,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
     
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
@@ -34,12 +39,28 @@
                for(NSDictionary* movie in self.movies){
                    NSLog(movie[@"title"]);
                }
+               
+               [self.tableView reloadData];
                // TODO: Get the array of movies
                // TODO: Store the movies in a property to use elsewhere
                // TODO: Reload your table view data
            }
        }];
     [task resume];
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.movies.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell* cell = [[UITableViewCell alloc] init];
+    
+    NSDictionary* movie = self.movies[indexPath.row];
+    cell.textLabel.text = movie[@"title"];
+    
+    return cell;
 }
 
 /*
